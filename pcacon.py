5d8f98ffd4b8b95a90bcac452b1f783638dcacab
灯明 玲音 <30884264+SuzuneToumyou@users.noreply.github.com>
2022-07-12 11:28:19 +0900

    Update pcacon.py

--- pcacon.py
+++ pcacon.py
@@ -5,38 +5,49 @@ import csv
 from sklearn.decomposition import PCA
 import numpy as np
 
-pass_data = "./data/"
-kari = "202206291042"
-format_a = ".csv"
+import sys
 
-file_name = pass_data + kari + format_a
+import pathlib as pl
+
+#pass_data = "./data/"
+#kari = "202206291042"
+#format_a = ".csv"
+
+#file_name = pass_data + kari + format_a
 
 keta = 250
 
-#print(file_name)
 
-def pca_convert():
+def pca_convert(pass_intmp):
 
-    csv_file = open(file_name, encoding="utf-8",  newline="" )
-    f = csv.reader(csv_file)
-    content = [row for row in f]
-    #print(content)
+    pass_intmp2 = "./" + pass_intmp
 
-    tP2 = sorted(content,reverse=True,key=lambda x: x[0])
-    del tP2[keta:]
-    #print(tP2)
+    p_temp = pl.Path(pass_intmp2).glob("*.csv")
+    for p in p_temp:
+        print(p)
 
-    pca = PCA(n_components = 1)
-    X2D =  pca.fit_transform(tP2)
-    X2D.shape
+        csv_file = open(p, encoding="utf-8",  newline="" )
+        f = csv.reader(csv_file)
+        content = [row for row in f]
+        
+        tP2 = sorted(content,reverse=True,key=lambda x: x[0])
+        del tP2[keta:]
 
-    #print (X2D)
+        pca = PCA(n_components = 1)
+        X2D =  pca.fit_transform(tP2)
+        X2D.shape
 
-    outfile_name = file_name[:-4] + "pca.csv"
+        outfile_name = str(p)[:-4] + "pca.csv"
 
-    fout = open(outfile_name, "w")
-    np.savetxt(fout,X2D,delimiter=",")
-    fout.close()
+        fout = open(outfile_name, "w")
+        np.savetxt(fout,X2D,delimiter=",")
+        fout.close()
 
 if __name__ == "__main__":
-    pca_convert()
+
+    if(len(sys.argv) <= 1):
+        pass_data = "data"
+    else:
+        pass_data = sys.argv[1]
+
+    pca_convert(pass_data)
